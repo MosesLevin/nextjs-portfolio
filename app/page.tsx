686bf2f7951751edab27ba7218172c07db9826e6
MosesLevin <moses.levin14@gmail.com>
2024-11-06 17:43:18 +0100

    added banner section with text&svg, completed optimization-section done.(no animations)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from './sections/Header'
 import Hero from './sections/Hero'
 import Projects from './sections/Projects'
+import Banner from './sections/Banner'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Header />
       <Hero />
       <Projects />
+      <Banner />
     </div>
   )
 }
